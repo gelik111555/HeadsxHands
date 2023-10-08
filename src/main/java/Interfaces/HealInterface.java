c34a92734bb21e77b4b1f76b4eb5080b3ba10c68
da4a <gelik111555@gmail.com>
2023-10-08 17:00:44 +0300

    feat:Added healing interface

--- src/main/java/Interfaces/HealInterface.java
+++ src/main/java/Interfaces/HealInterface.java
@@ -1,2 +1,9 @@
-package Interfaces;public interface HealInterface {
+package Interfaces;
+
+import Models.HealResult;
+
+public interface HealInterface {
+    public HealResult superHeal();
+    public int getHealingCount();
+    public int getMaxHealingCount();
 }
